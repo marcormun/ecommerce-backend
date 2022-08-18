78e4b8992a21ca7c7cbbac3c2c12808aefb6bec6
marcormun <mcordontrabajo@gmail.com>
2022-08-19 00:11:02 +0200

    feat: index.js + config added for db connection, CRUD folders created

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+require('dotenv').config();
+const db = require('./config/database');
+
+//middleware
+app.use(express.json())
+
+//cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+//port
+const PORT = process.env.PORT || 4000;
+
+//routes
+
+app.get('/', (req, res)=> {
+    return res.send('Bienvenidos al backend de e-sneakers');
+});
+
+app.get('*',(req,res)=>{
+    return res.status(404).send('404 route not found')
+})
+
+//connect database
+
+db().then(()=>{
+    app.listen(PORT, ()=> console.log('Server is running in ' + PORT));
+    }).catch((error)=>{
+        console.log('Error connecting to mongoDB ', error);
+    })
+    
