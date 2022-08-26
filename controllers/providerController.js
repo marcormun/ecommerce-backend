09e62e4de857d17fe719ff4bfb61ccc82be150d2
marcormun <mcordontrabajo@gmail.com>
2022-08-27 00:09:39 +0200

    feat: put/delete added to providers

--- controllers/providerController.js
+++ controllers/providerController.js
@@ -110,12 +110,43 @@ providerController.createProvider = async (req, res) => {
 providerController.modifyProvider = async (req, res) => {
     try{
         const filter = {_id: req.params.id};
-        const user = await Provider.findByIdAndUpdate(req.params.id);
+        
+        const provider = await Provider.findByIdAndUpdate(req.params.id);
+        
         const update = {
             name: req.body.name,
             phoneNumber: req.body.phoneNumber
         };
+        if(req.user_role!='admin'){
+            return res.status(404).json(
+                {
+                    success: true,
+                    message: "You don't have permission to update providers"
+                }
+            );
+        }
+
+        if(!provider){
+            return res.status(404).json(
+                {
+                    success: true,
+                    message: "Provider not found"
+                }
+            );
+        }
         
+        await Provider.findOneAndUpdate(filter, update);
+        
+        const providerUpdated = await Provider.findOne(filter).select(['-password','-__v']);
+        
+        return res.status(200).json(
+            {
+                success: true,
+                message: "Provider updated succesfully",
+                data: providerUpdated
+            }
+        );
+
     }catch(error){
         if(error?.message.includes('Cast to ObjectId failed')) {
             return res.status(404).json(
@@ -135,5 +166,36 @@ providerController.modifyProvider = async (req, res) => {
         );  
     }
 }
+
+providerController.deleteProviderById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const providerDeleted = await Provider.findByIdAndDelete(id).select(['-password','-__v']);
+        if(req.user_role!='admin'){
+            return res.status(404).json(
+                {
+                    success: true,
+                    message: "You don't have permission to delete providers"
+                }
+            );
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Delete provider sucessfully",
+            data: providerDeleted
+        })
+
+    
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'Error deleting provider',
+                data: error?.message ||error
+            }
+        )
+    }
+};
 
 module.exports = providerController;
