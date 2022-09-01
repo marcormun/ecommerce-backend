d8493bb7996235e4629958c5d0d4cfdf58bfac17
marcormun <mcordontrabajo@gmail.com>
2022-09-01 03:18:06 +0200

    feat: modifyOrder and deleteOrder added to orderController

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -84,4 +84,73 @@ orderController.addOrder = async (req,res) => {
     }
 }
 
+orderController.modifyOrderStatusById = async (req,res) => {
+    try{
+        const filter = {_id: req.params.id};
+        const order = await Order.findById(req.params.id);
+        const update = {
+            status: req.body.status
+        }
+        if(!order) {
+            return res.status(404).json(
+                {
+                    success: true,
+                    message: "Order not found",
+                    data: []
+                }
+            ); 
+        }
+        await Order.findOneAndUpdate(filter, update);
+        const orderUpdated = await Order.findOne(filter).select(['-__v']);
+        return res.status(200).json(
+            {
+                success: true,
+                message: "Status updated succesfully",
+                data: orderUpdated
+            }
+        );
+    }catch(error){
+        if(error?.message.includes('Cast to ObjectId failed')) {
+            return res.status(404).json(
+                {
+                    success: true,
+                    message: "Order not found"
+                }
+            )
+        };
+
+        return res.status(500).json(
+            {
+                success: false,
+                message: "Error finding order",
+                error: error?.message || error
+            }
+        );   
+    }  
+}
+
+
+orderController.deleteOrderById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const orderDeleted = await Order.findByIdAndDelete(id).select(['-__v']);
+
+        return res.status(200).json({
+            success: true,
+            message: "Delete order sucessfully",
+            data: orderDeleted
+        })
+
+    
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'Error deleting order',
+                data: error?.message ||error
+            }
+        )
+    }
+};
+
 module.exports = orderController;
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,6 +6,6 @@ const verifyToken = require('../middlewares/verifyToken');
 router.get('/orders', verifyToken, isAdmin, orderController.getAllOrders);
 router.get('/orders/user/:id', verifyToken, orderController.getAllOrdersByUser);
 router.post('/orders', orderController.addOrder);
-// router.put('/orders/id/:id', orderController.modifyOrderStockById);
-// router.delete('/orders/id/:id',orderController.deleteOrderById);
+router.put('/orders/id/:id', orderController.modifyOrderStatusById);
+router.delete('/orders/id/:id',orderController.deleteOrderById);
 module.exports = router;
