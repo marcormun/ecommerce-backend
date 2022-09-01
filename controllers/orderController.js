25b8c500eea721b84cc4b0b0cf1a69cc88c6a2c0
marcormun <mcordontrabajo@gmail.com>
2022-09-01 03:12:21 +0200

    feat: addOrder added to orderController

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -24,5 +24,64 @@ orderController.getAllOrders = async (req,res) => {
     }
 };
 
+orderController.getAllOrdersByUser = async (req,res) => {
+    try {
+        const orders = await Order.find({userId: req.params.id});
+        
+        return res.status(200).json(
+            {
+                success: true,
+                message: 'Get all orders retrieved succsessfully',
+                data: orders
+            }
+        );
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'Error retrieving orders',
+                error: error.message
+            }
+        )
+    }
+}
+
+orderController.addOrder = async (req,res) => {
+    try{
+        const {orderDate, status, userId, productId} = req.body;
+        
+        if(!orderDate || !status || !userId || !productId){
+            return res.status(400).json({
+                success: false,
+                message: 'missing fields'
+            });
+        }
+
+        const newOrder = {
+            orderDate,
+            status,
+            userId,
+            productId
+        }
+
+        await Order.create(newOrder);
+
+        return res.status(200).json(
+            {
+                success: true,
+                message: 'Order created succesfully',
+                data: newOrder
+            }
+        )
+    }catch(error){
+        return res.status(500).json(
+            {
+                success: false,
+                message: "Error adding order"
+                
+            }
+        )
+    }
+}
 
 module.exports = orderController;
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,6 +3,9 @@ const orderController = require('../controllers/orderController');
 const isAdmin = require('../middlewares/isAdmin');
 const verifyToken = require('../middlewares/verifyToken');
 
-router.get('/providers', verifyToken, isAdmin, orderController.getAllOrders);
-
+router.get('/orders', verifyToken, isAdmin, orderController.getAllOrders);
+router.get('/orders/user/:id', verifyToken, orderController.getAllOrdersByUser);
+router.post('/orders', orderController.addOrder);
+// router.put('/orders/id/:id', orderController.modifyOrderStockById);
+// router.delete('/orders/id/:id',orderController.deleteOrderById);
 module.exports = router;
