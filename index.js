d041322b5a14f658884880a7210a99011de9550c
marcormun <mcordontrabajo@gmail.com>
2022-09-06 00:08:01 +0200

    fix: authRoutes authController minusculas en la relacion

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ app.get('/', (req, res)=> {
 
 app.get('*',(req,res)=>{
     return res.status(404).send('404 route not found')
-})
+});
 
 //connect database
 
 db().then(()=>{
     app.listen(PORT, ()=> console.log('Server is running in ' + PORT));
-    }).catch((error)=>{
-        console.log('Error connecting to mongoDB ', error);
-    })
+}).catch((error)=>{
+    console.log('Error connecting to mongoDB ', error);
+})
     
